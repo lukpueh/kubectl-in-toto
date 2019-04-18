825cfc3eebb9911ed85e54fd76b8387a435a9bfc
Lukas Puehringer <lukas.puehringer@nyu.edu>
2019-04-18 10:59:01 +0200

    Fix/enhance layout creation script
    - Replace shlex and direct assignment with handy helpers
    - Fix false rule assignment to non-existent `products` field (it
    is called `expected_products`.
    - Enhance rule set

--- example/project-owner/create_layout.py
+++ example/project-owner/create_layout.py
@@ -3,7 +3,7 @@
 import in_toto.models.layout as l
 import in_toto.models.metadata as m
 import in_toto.util as util
-import shlex
+
 
 # keys = util.import_gpg_public_keys_from_keyring_as_dict(['functionary.pub'])
 project_owner = util.import_rsa_key_from_file('project-owner')
@@ -13,16 +13,19 @@ keyid = layout.add_functionary_key_from_path('functionary.pub')['keyid']
 
 step = l.Step()
 step.name = 'build'
-step.materials = []
-step.products = ['CREATE', 'image_id']
-step.expected_command = shlex.split('docker build . -t empty --idfile image_id')
+step.expected_products = [['REQUIRE', 'image_id']]
+step.set_expected_command_from_string(
+    'docker build . -t empty --idfile image_id')
 step.pubkeys = [keyid]
 
 inspection = l.Inspection()
 inspection.name = 'verify-image'
-inspection.expected_materials = [['MATCH', 'image_id', 'WITH', 'PRODUCTS', 'FROM', 'build']]
-inspection.expected_products = []
-inspection.run = shlex.split("echo", "lol")
+inspection.expected_materials = [
+    ['REQUIRE', 'image_id'],
+    ['MATCH', 'image_id', 'WITH', 'PRODUCTS', 'FROM', 'build'],
+    ['DISALLOW', '*']
+  ]
+inspection.set_run_from_string('echo lol')
 
 layout.steps.append(step)
 layout.inspect.append(inspection)
